e2c0d9118d2a795915a54858f9b10d4fe35dd78f
Zaid Khan <zaid69737@gmail.com>
2025-05-18 18:46:26 +0530

    Add files via upload

---
+++ 1_Datatypes.py
@@ -0,0 +1,59 @@
+# STRING 
+
+a='23 Zaid Ali'
+b='Greeting from Me'
+print(a)
+print(b)
+
+
+# INTEGER
+
+a=30
+b=29
+print('This is Integer',a )
+
+
+# FLOAT
+
+a=30.7
+print('This is FLoat',a)
+
+
+# BOOLEAN VALUE
+
+a=20
+b=290
+z=a==b
+print(z)
+
+
+## LIST [ ✔ ORDERED ,✔ Allow Changeble ,✔ Duplication ]
+
+a=['10','20','auhd','10','20']
+print('This is List ',a)
+print('This is List ',a[1:5])
+
+
+## TUPLE ( ✔ ORDERED ,❌ Allow Changeble ,✔ Duplication)
+
+a=('30','33','54')
+print('This is the TUPLE',a)
+print('This is the TUPLE',a[2])
+
+
+## SET { ❌ ORDERED ,❌ Allow Changeble ,❌ Duplication}
+
+a={'za','id',' ','Kh','an'}
+print(a)
+
+
+## DICTIONARY {✔ ORDERED ,✔ Allow Changeble ,❌ Duplication}
+
+business={'Name':'E-Commerce',
+          'Platform':'MY Website',
+          'Owner':'Zaid Ali',
+          'Location':'India',
+          'Required':'Money'}
+
+print(business)
+print(business['Name'])
