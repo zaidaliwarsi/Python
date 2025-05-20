5ca851dcf0e4a2bdd881db1d4a27aa1863c12a0b
Zaid Khan <zaid69737@gmail.com>
2025-05-20 21:54:11 +0530

    Add files via upload

---
+++ CalculatorApp.py
@@ -0,0 +1,50 @@
+print("------------------Calculator App---------------")
+
+print("\n")
+print("Select Any one One Arithematic Operation to PErform: ")
+print(" 1. Addition\n 2. Multiplication\n 3. Division\n 4. Substraction ")
+choice= input(" ► Select any One Number (  1 / 2 / 3 / 4  ) : ")
+
+if choice=="1":
+
+
+    def add():
+        print("\n------Addition Program-------")
+        print("\n")
+        x = int(input("Enter First Number: "))
+        y = int(input("Enter Second Number: "))
+        z=x+y
+        return z
+    print("The Addition of 2 Numbers is : ",add())
+
+if choice=="2":
+
+    def mul():
+        print("\n------Multiplication Program-------")
+        print("\n")
+        a = int(input("ENTER FIRST NO. : "))
+        b = int(input("ENTER SECOND NO. : "))
+        c = a*b
+        return c
+    print("The Multiplication of Both Numbers are : ",mul())
+
+if choice == "3":
+
+    def div():
+        print("\n------Division Program-------")
+        print("\n")
+        d = int(input("ENTER FIRST NO. : "))
+        e = int(input("ENTER SECOND NO. : "))
+        f = d/e
+        return f
+    print("The Division of Both Numbers are : ",div())
+
+if choice == "4":
+    def sub():
+        print("\n------Substraction Program-------")
+        print("\n")
+        g = int(input("ENTER FIRST NO. : "))
+        h = int(input("ENTER SECOND NO. : "))
+        i = g-h
+        return i
+    print("The Substraction of Both Numbers are : ",sub())
